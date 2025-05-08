402c870a16f3f1f24e1b1917834cf33f5b94dae4
Gerrit Riessen <gerrit@openmindmap.org>
2025-05-08 17:49:35 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# erlang-red-unittesting-nodes
+## Unit testing nodes for Erlang-RED
+
+Nodes for testing flow correctness in the [Erlang-RED](https://github.com/gorenje/erlang-red) project.
+
+These could be thought of the first Erlang-RED-first nodes! They provided limited functionality within Node-RED as they deal with testing of node functionality and nodes are assumed to work in Node-RED.
+
+
---
+++ nodes/ut-assert-values.js
@@ -0,0 +1,120 @@
+module.exports = function(RED) {
+  function CoreutassertvaluesFunctionality(config) {
+    RED.nodes.createNode(this,config);
+
+    var node = this;
+    var cfg = config;
+
+
+    var sendToDebug = (nde,rule,msgc,lvl) => {
+     try {
+      let msg = {
+        id:     nde.id,
+        z:      nde.z,
+        _alias: nde._alias,
+        path:   nde._flow.path,
+        name:   nde.name,
+        topic:  msgc.topic,
+        msg:    {msg: msgc, rule: rule},
+        level:  lvl
+      }
+
+      msg = RED.util.encodeObject(msg);
+      RED.comms.publish("debug", msg);
+    } catch (ex) {
+      console.error(ex)
+    }
+    return false;
+  }
+
+  var postUnsupported = (rule) => {
+    node.status({ fill: "red", shape: "dot", 
+                    text: RED._("ut-assert-values.label.unsupported", { property: JSON.stringify(rule) }) });
+    sendToDebug(node, rule, 30)
+    return rule
+  }
+
+    node.on('close', function() {
+      node.status({});
+    });
+
+    /* msg handler, in this case pass the message on unchanged */
+    node.on("input", function(msg, send, done) {
+        var failures = [];
+        var unsupported = [];
+
+        cfg.rules.forEach(rule => {     
+          /*
+           * Rule is Equal
+           */   
+          if ( rule.t == "eql" && rule.pt == "msg") {
+            if ( rule.tot == "str") {
+              if ( rule.to != RED.util.getObjectProperty(msg,rule.p) ) {
+                failures.push(sendToDebug(node, rule, msg, 20))
+              }
+            } else if ( rule.tot == "num") {
+              if ( parseInt(rule.to) != parseInt(RED.util.getObjectProperty(msg,rule.p)) ) {
+                failures.push(sendToDebug(node, rule, msg, 20))                
+              }
+            } else if (rule.tot == "json") {
+              let expObj = JSON.parse(rule.to)
+              let oldObj = RED.util.getObjectProperty(msg, rule.p)
+              if ( JSON.stringify(oldObj, Object.keys(oldObj).sort()) != JSON.stringify(expObj, Object.keys(expObj).sort()) ) {
+                failures.push(sendToDebug(node, rule, msg, 20))                
+              }
+            } else {
+              unsupported.push(postUnsupported(rule))
+            }
+          /*
+           * Rule is match
+           */
+          } else if ( rule.t == "mth" && rule.pt == "msg") {
+            if (rule.tot == "str") {
+              let reExp = new RegExp(rule.to)
+              if ( !reExp.test(RED.util.getObjectProperty(msg, rule.p)) ) {
+                failures.push(sendToDebug(node, rule, msg, 20))                
+              }
+            } else {
+               unsupported.push(postUnsupported(rule))
+            }
+          /*
+           * Other rule types are not supported
+           */
+          } else {
+             unsupported.push(postUnsupported(rule))
+          }
+        
+          console.log(rule)
+        })
+
+        if (failures.length > 0 ) {
+          node.status({fill: "red", shape: "dot", text: "assert failed"})
+        } else {
+          if ( unsupported.length > 0) {
+            node.status({ fill: "yellow", shape: "ring", text: "unsupported errors" })
+          } else {
+            node.status({ fill: "green", shape: "ring", text: "assert succeed" })
+          }
+        }
+        
+        try {
+          try {
+            send(msg);
+            done();
+          } catch ( err ) {
+            // use node.error if the node might send subsequent messages
+            node.error("error occurred", { ...msg, error: err })
+            done();
+          }
+        } catch (err) {
+          // use done if the node won't send anymore messages for the
+          // message it received.
+          msg.error = err
+          done(err.message, msg)
+        }
+    });
+  }
+
+  RED.nodes.registerType("ut-assert-values", CoreutassertvaluesFunctionality);
+
+}
