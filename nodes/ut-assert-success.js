345d2a1435ad92ac29426472fab2069eb4b41f76
Gerrit Riessen <gerrit@openmindmap.org>
2025-08-19 14:58:01 +0200

    added counter on the assert true node

--- nodes/ut-assert-success.js
+++ nodes/ut-assert-success.js
@@ -5,6 +5,39 @@ module.exports = function(RED) {
     var node = this;
     var cfg = config;
 
+
+    var sendToDebug = (nde, rule, msgc, lvl) => {
+      try {
+        let msg = {
+          id: nde.id,
+          z: nde.z,
+          _alias: nde._alias,
+          path: nde._flow.path,
+          name: nde.name,
+          topic: msgc.topic,
+          msg: { msg: msgc, rule: rule },
+          level: lvl
+        }
+
+        msg = RED.util.encodeObject(msg);
+        RED.comms.publish("debug", msg);
+      } catch (ex) {
+        console.error(ex)
+      }
+      return ["failure", rule];
+    }
+
+    var postUnsupported = (rule, msg) => {
+      node.status({
+        fill: "red", shape: "dot",
+        text: RED._("ut-assert-success.label.unsupported", { property: JSON.stringify(rule) })
+      });
+      sendToDebug(node, rule, msg, 30)
+
+      return ["unsupported", rule]
+    }
+
+
     node.on('close', function() {
       node.status({});
     });
@@ -12,8 +45,12 @@ module.exports = function(RED) {
     /* msg handler, in this case pass the message on unchanged */
     node.on("input", function(msg, send, done) {
         // How to send a status update
-      node.status({ fill: "green", shape: "ring", text: RED._("ut-assert-success.label.succeed") });
-
+        if ( cfg.count == 1) {
+          node.status({ fill: "green", shape: "ring", text: RED._("ut-assert-success.label.succeed") });
+        } else {
+          postUnsupported("count != 1", msg)
+          node.status({ fill: "red", shape: "ring", text: RED._("ut-assert-success.label.failed") });
+        }
         // Send a message and how to handle errors.
         try {
           try {
