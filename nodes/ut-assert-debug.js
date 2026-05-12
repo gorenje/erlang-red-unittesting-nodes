9cf367e83f5875418022688d820674e40687c3c1
Gerrit Riessen <gerrit@openmindmap.org>
2026-05-12 11:30:20 +0200

    initial setup for running tests in Node RED

--- nodes/ut-assert-debug.js
+++ nodes/ut-assert-debug.js
@@ -36,4 +36,106 @@ module.exports = function(RED) {
 
   RED.nodes.registerType("ut-assert-debug", Coreut_assert_debugFunctionality);
 
+  // Backend endpoints
+RED.httpAdmin.get("/UnitTesting/:flowid/runtest",
+  (req, res) => {
+    const path = require('path');
+    const fs = require('fs')
+    let testDir = path.resolve(path.dirname(__filename), "..", "testflows")
+
+    res.sendStatus(200);
+
+    var runtime = require("@node-red/runtime");
+
+    if (req.params.flowid == "all") {
+      fs.globSync(`${testDir}/**/*.json`).forEach(filename => {
+        RED.comms.publish("unittesting:testresults", {
+          flowid: path.basename(path.parse(filename).dir),
+          status: "success"
+        })
+      })
+    } else {
+      fs.globSync(`${testDir}/**/*.json`).filter(d => d.includes(req.params.flowid) ).forEach(filename => {
+        let flowDetails = JSON.parse(fs.readFileSync(filename))
+        let flowId = req.params.flowid
+
+        let tabNode = flowDetails.filter( d => d.type == "tab")[0]
+        let nonTabNodes = flowDetails.filter(d => d.type != "tab")
+
+        let details = {
+          ...tabNode,
+          nodes: nonTabNodes,
+        }
+
+        /*
+        runtime._.flows.updateFlow(flowId, details, "root").then( _ignore => {
+          runtime._.flows.removeFlow(flowId, "root").then( _alsoIgnore => {
+            RED.comms.publish("unittesting:testresults", {
+              flowid: path.basename(path.parse(filename).dir),
+              status: "success"
+            })
+          })
+        })
+        */
+        RED.comms.publish("unittesting:testresults", {
+          flowid: path.basename(path.parse(filename).dir),
+          status: "success"
+        })
+      })
+    }
+  });
+
+RED.httpAdmin.get("/UnitTesting/halt",
+  (req, res) => {
+    res.sendStatus(200);
+  });
+
+RED.httpAdmin.get("/UnitTesting/:flowid/retrieve",
+  (req, res) => {
+    const path = require('path');
+    const fs = require('fs')
+
+    let fileName = path.resolve(path.dirname(__filename), "..", "testflows", req.params.flowid, "flows.json")
+    const data = fs.readFileSync(fileName);
+
+    try {
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify({ flowdata: JSON.parse(data) }));
+    } catch (err) {
+      res.sendStatus(500);
+    }
+  });
+
+RED.httpAdmin.get("/UnitTesting/tests.json",
+  (req, res) => {
+    let testData = {
+      "status": "ok",
+      "last_updated_at": "2025-04-09T14:54:01.665Z",
+      "data": {
+      }
+    }
+
+    const path = require('path');
+    const fs = require('fs')
+
+    let testDir = path.resolve(path.dirname(__filename), "..", "testflows")
+
+    fs.globSync(`${testDir}/**/*.json`).forEach(filename => {
+      let details = path.parse(filename)
+      const data = fs.readFileSync(filename);
+
+      testData.data[path.basename(details.dir)] = {
+        "id": path.basename(details.dir),
+        "name": JSON.parse(data).filter(d => d.type == "tab")[0].label
+      }
+    })
+
+    try {
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify(testData));
+    } catch (err) {
+      res.sendStatus(500);
+    }
+  });
+
 }
